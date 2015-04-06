a92d0e830056758ce3a9bdeb65e25b591113efe2
Andy Russell <arussell123@gmail.com>
2015-04-05 19:47:06 -0700

    authorize server with firebase secret

--- database.js
+++ database.js
@@ -2,5 +2,12 @@ var config = require('./config');
 
 var Firebase = require('firebase');
 var firebase = new Firebase(config.get('FIREBASE_URL'));
+firebase.authWithCustomToken(config.get('FIREBASE_SECRET'), function(err) {
+    if (err) {
+        throw new Error('Login to firebase failed.');
+    } else {
+        console.log('Successfully logged into firebase.');
+    }
+});
 
 module.exports = firebase;
